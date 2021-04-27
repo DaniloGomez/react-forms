98d2881927bffd29b55e6e69a61969f124339756
DaniloGomez <danilogomez8@hotmail.com>
2021-04-27 18:15:38 -0500

    send first

---
+++ src/pages/components/store/store.comp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '/Danilo/react-forms/src/pages/components/store/store.style.css'
+
+function StorePage() {
+  return (
+    <div className="storepage">
+      Store Page
+    </div>
+  );
+}
+
+export default StorePage;
---
+++ src/pages/firstpage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import StorePage from './components/store/store.comp';
+
+function First() {
+  return (
+    <div className="first">
+      <StorePage/>
+    </div>
+  );
+}
+
+export default First;
