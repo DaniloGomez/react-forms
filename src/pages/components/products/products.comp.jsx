1b275e76ab2653df392f5af7af76cd08fc151aec
DaniloGomez <danilogomez8@hotmail.com>
2021-04-27 23:14:18 -0500

    project

---
+++ src/pages/components/products/products.comp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../products/products.style.css';
+
+function ProductsPage() {
+  return (
+    <div className="productpage">
+      <u><b>Products list</b></u>
+    </div>
+  );
+}
+
+export default ProductsPage;
--- src/pages/components/store/store.comp.jsx
+++ src/pages/components/store/store.comp.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import '/Danilo/react-forms/src/pages/components/store/store.style.css'
+import ProductsPage from '../products/products.comp';
 
 function StorePage() {
   return (
     <div className="storepage">
-      Store Page
+      <b><u>Store list</u></b>
+      <ProductsPage/>
     </div>
   );
 }
+
 
 export default StorePage;
